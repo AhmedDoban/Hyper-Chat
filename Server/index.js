e7a2158732f87e2cc08421fd2d63666b12456144
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-12-13 01:41:44 +0200

    update the basic connection to socket io backend side

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
 import Users_Routes from "./App/Routes/Users_Routes.js";
 import Request_Routes from "./App/Routes/Request_Routes.js";
 import Message_Routes from "./App/Routes/Message_Routes.js";
@@ -14,6 +16,7 @@ mongoose
 
 // middlewares
 const App = express();
+
 App.use(express.json());
 App.use(cors());
 
@@ -29,6 +32,27 @@ App.use("*", (Req, Res) => {
 });
 
 // Server
-App.listen(4000, () => {
+const AppServer = http.createServer(App);
+
+const io = new Server(AppServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+global.OnlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.Chatsocket = socket;
+  socket.on("online_user", (userId) => {
+    const user = OnlineUsers.set(userId, socket.id);
+  });
+  socket.on("Send_Message", (data) => {
+    const GETUSerSocket = OnlineUsers.get(data.To);
+    socket.to(GETUSerSocket).emit("Recive_Message", data.Message);
+  });
+});
+
+AppServer.listen(4000, () => {
   console.log("Listen in port 4000");
 });
